256ad77aed235e636e87cf6a39638ebc11a388f0
Daniel Covelli <57047397+daniel-covelli@users.noreply.github.com>
2024-08-13 11:19:24 -0700

    add api layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { getApplications } from "@/utils/api";
 
 
-export default function HomePage() {
+export default async function HomePage() {
+  const applications = await getApplications()
+
+  console.log('applications', JSON.stringify(applications, null, 2));
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
---
+++ src/utils/api.ts
@@ -0,0 +1,28 @@
+import { env } from "@/env";
+
+const BASE_URL = 'https://harvest.greenhouse.io/v1';
+const AUTH_TOKEN = env.GREEN_HOUSE_API_KEY
+
+console.log("AUTH_TOKEN", btoa(AUTH_TOKEN))
+
+const headers = {
+  'Authorization': `Basic ${btoa(AUTH_TOKEN + ":")}`,
+  'Content-Type': 'application/json',
+};
+
+async function fetchWrapper(route: string, options: RequestInit = {}): Promise<unknown> {
+  try {
+    const response = await fetch(`${BASE_URL}${route}`, { ...options, headers: { ...headers, ...options.headers } });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json() as unknown
+  } catch (error) {
+    console.error(`Error fetching from ${BASE_URL}${route}:`, error);
+    throw error;
+  }
+}
+
+export const getApplications = () =>
+  fetchWrapper("/applications");
